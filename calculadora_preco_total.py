5be31a70b50c4c7e0cce32a690e9455956eba4dd
Marlon Ferreira <mferreirab03@gmail.com>
2025-07-22 10:29:36 -0300

    commit de verificação

---
+++ calculadora_preco_total.py
@@ -0,0 +1,5 @@
+nameProduct = "Cadeira Infantil"
+price = 12.40
+qtd = 3
+
+print("O preço total do produto", nameProduct, "é:", price * qtd)
---
+++ calucladora_volume.py
@@ -0,0 +1,5 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+print("O volume da caixa retangular é:", comprimento * largura * altura)
